ccbf80f903c5588d1ab12e3f80a1a5fb533f6de5
Jeff Caten <jeffcaten@gmail.com>
2020-06-01 09:45:25 -0500

    Added Log output.  Cleaned up AWS CLI for readability. Moved commented AWS regions to the bottom of the array.

--- AWS/listRunningEC2-RDS.sh
+++ AWS/listRunningEC2-RDS.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+logFileLocation="./listRunningEC2-RDS.txt"
+
 # AWS region array
 array=(
 "us-east-2"
@@ -9,14 +11,11 @@ array=(
 "af-south-1"
 "ap-east-1"
 "ap-south-1"
-#"ap-northeast-3"
 "ap-northeast-2"
 "ap-southeast-1"
 "ap-southeast-2"
 "ap-northeast-1"
 "ca-central-1"
-#"cn-north-1"
-#"cn-northwest-1"
 "eu-central-1"
 "eu-west-1"
 "eu-west-2"
@@ -25,19 +24,29 @@ array=(
 "eu-north-1"
 "me-south-1"
 "sa-east-1"
+#"cn-north-1"
+#"cn-northwest-1"
+#"ap-northeast-3"
 "${arr[@]}")
 
-echo "Looking for running EC2 instances"
+echo "Looking for running EC2 instances" | tee -a $logFileLocation
 for region in "${array[@]}"
 do
-	echo $region
-    aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,State.Name,InstanceType,KeyName,LaunchTime]' --filters Name=instance-state-name,Values=running --output table --region $region
+	echo $region  | tee -a $logFileLocation
+    aws ec2 describe-instances \
+        --query 'Reservations[*].Instances[*].[InstanceId,State.Name,InstanceType,KeyName,LaunchTime]' \
+        --filters Name=instance-state-name,Values=running \
+        --output table \
+        --region $region | tee -a $logFileLocation
 done
 
-echo "Looking for running RDS instances"
+echo "Looking for running RDS instances" | tee -a $logFileLocation
 for region in "${array[@]}"
 do
-	echo $region
+	echo $region  | tee -a $logFileLocation
     #aws rds describe-db-instances --output table --region $region
-    aws rds describe-db-instances --query 'DBInstances[?DBInstanceStatus==`available`].[DBInstanceIdentifier,DBInstanceStatus,DBInstanceClass,InstanceCreateTime]' --output table --region $region
+    aws rds describe-db-instances \
+    --query 'DBInstances[?DBInstanceStatus==`available`].[DBInstanceIdentifier,DBInstanceStatus,DBInstanceClass,InstanceCreateTime]' \
+    --output table \
+    --region $region | tee -a $logFileLocation
 done
